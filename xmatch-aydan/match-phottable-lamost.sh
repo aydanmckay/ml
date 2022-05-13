4612fc64ed702263c646d236f9e3420a9d51f286
Aydan McKay <aydanmckay@uvic.ca>
2022-05-13 23:26:10 +0000

    xmatched LAMOST with phot-table

--- xmatch-aydan/match-phottable-lamost.sh
+++ xmatch-aydan/match-phottable-lamost.sh
@@ -4,17 +4,17 @@
 scratch=/scratch
 stilts="stilts -Xmx150G -Djava.io.tmpdir=${scratch} -verbose"
 
-phots=/arc/home/aydanmckay/pristine-xmatch-phottable-exact.fits
-lamost=/arc/projects/k-pop/pristine/xmatch/
+phots=/arc/home/aydanmckay/pristine-phottable-exact.fits
+lamost=/arc/projects/k-pop/pristine/xmatch/pristine_lamost_dr8_gaiaedr3.fits
 outfile=/arc/home/aydanmckay/pristine-phottable-lamost.fits
 
 ${stilts} tmatch2 \
         in1=${phots} \
-        values1='ra dec CaHK' \
+        values1='source_id' \
         in2=${lamost} \
-        icmd2="delcols 'e_CaHK flag '" \
-        values2='RA(CaHK) Dec(CaHK) CaHK' \
-        matcher=exact+exact+exact \
+        icmd2="delcols 'RA(CaHK) Dec(CaHK) CaHK_0 field_nb ccd_nb X_1 Y_1 e_CaHK flag E_BV parallax parallax_error pmra pmra_error pmdec pmdec_error phot_g_mean_mag phot_bp_mean_mag phot_rp_mean_mag dr2_radial_velocity dr2_radial_velocity_error phot_g_mean_mag_error phot_bp_mean_mag_error phot_rp_mean_mag_error'" \
+        values2='source_id' \
+        matcher=exact \
         join=1and2 \
-        ocmd="delcols 'RA(CaHK) Dec(CaHK) CaHK_2'; colmeta -name CaHK CaHK_1" \
+        ocmd="delcols 'source_id_2 source_id_1'" \
         out=${outfile}
