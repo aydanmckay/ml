dd6056167ec10e9a2e5f2199129bb6b9bd052653
Aydan McKay <aydanmckay@uvic.ca>
2022-05-13 01:22:41 +0000

    added pan-starrs dr1 to phot-table

--- xmatch-aydan/match-phottable-lamost.sh
+++ xmatch-aydan/match-phottable-lamost.sh
@@ -5,14 +5,14 @@ scratch=/scratch
 stilts="stilts -Xmx150G -Djava.io.tmpdir=${scratch} -verbose"
 
 phots=/arc/home/aydanmckay/pristine-xmatch-phottable-exact.fits
-pans=/arc/home/aydanmckay/xmatch/pristine_panstarrs_dr1.fits
-outfile=/arc/home/aydanmckay/pristine-phottable-exact.fits
+lamost=/arc/projects/k-pop/pristine/xmatch/
+outfile=/arc/home/aydanmckay/pristine-phottable-lamost.fits
 
 ${stilts} tmatch2 \
         in1=${phots} \
         values1='ra dec CaHK' \
-        in2=${pans} \
-        icmd2="delcols 'e_CaHK flag objID RAJ2000 DEJ2000 errHalfMaj errHalfMin errPosAng f_objID Epoch Ns Nd gKmag e_gKmag rKmag e_rKmag iKmag e_iKmag zKmag e_zKmag yKmag e_yKmag'" \
+        in2=${lamost} \
+        icmd2="delcols 'e_CaHK flag '" \
         values2='RA(CaHK) Dec(CaHK) CaHK' \
         matcher=exact+exact+exact \
         join=1and2 \
